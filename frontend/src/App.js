1d779a0059d909d087676bad255dea600604788c
Guillaume <gservais98@gmail.com>
2019-07-17 10:39:38 +0200

    update routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 import PaintingDetail from './components/painting-detail/painting-detail';
 import NavBar from './components/navbar/navbar';
 import ChooseLanguage from '../src/components/choose-language/choose-language';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ChoosePainting from '../src/components/choose-painting/choose-painting'
+import { BrowserRouter, Route, Switch, Redirect, Router } from 'react-router-dom';
+import { createBrowserHistory } from "history";
+import ChoosePainting from '../src/components/choose-painting/choose-painting';
+
+const history = createBrowserHistory();
 
  class App extends React.Component{
    constructor(props){
@@ -15,13 +18,13 @@ import ChoosePainting from '../src/components/choose-painting/choose-painting'
     return (
       <div className="App">
         {/* <NavBar></NavBar> */}
-        <BrowserRouter>
+        <Router history={history}>
           <Switch>
-            <Route path="/language" component={ChooseLanguage}></Route>
+            <Route exact={true} path="/" component={ChooseLanguage}></Route>
             <Route path="/choose-painting" component={ChoosePainting}></Route>
             <Route path="/paintings/detail/:id" component={PaintingDetail}></Route>
           </Switch>
-        </BrowserRouter>
+        </Router>
       </div>
     );
   }
